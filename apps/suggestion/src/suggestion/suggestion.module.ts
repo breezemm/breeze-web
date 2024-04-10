071d7d999fa8d6baa696d3f059cead98356287f3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-04-10 22:21:36 +0630

    refactor: remove suggestion service

--- apps/suggestion/src/suggestion/suggestion.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SuggestionService } from './suggestion.service';
-import { SuggestionController } from './suggestion.controller';
-
-@Module({
-  controllers: [SuggestionController],
-  providers: [SuggestionService],
-})
-export class SuggestionModule {}
