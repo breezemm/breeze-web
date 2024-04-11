a9e587bcfd7862ac73759ad27423b9888f95f33a
Kaung Myat Thu <kaungmyatthu692017@gmail.com>
2024-04-11 11:07:30 +0630

    feat: add home timeline

---
+++ apps/app/components/DownloadDialogBox.tsx
@@ -0,0 +1,36 @@
+
+import {
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogTrigger,
+} from "@breeze/ui";
+
+import AppleStore from "@/app/assets/apple_store.svg";
+import GoogleStore from "@/app/assets/google_store.svg";
+import Image from "next/image";
+import { cn } from "@/utils";
+
+const DownloadDialogBox = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger className={cn("", className)}>{children}</DialogTrigger>
+      <DialogOverlay className="bg-overlaycolor  backdrop-blur-sm" />
+
+      <DialogContent
+        style={{ borderRadius: "1rem" }}
+        className="w-80  flex justify-center  flex-col items-center gap-12 p-16"
+      >
+      Login
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DownloadDialogBox;
---
+++ apps/app/components/TimelineNavbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import HomeIcon from "@/app/assets/icons/HomeIcon";
+import SearchIcon from "@/app/assets/icons/SearchIcon";
+import EventIcon from "@/app/assets/icons/EventIcon";
+import NotificationIcon from "@/app/assets/icons/NotiIcon";
+import UserProfileIcon from "../app/assets/icons/UserProfileIcon";
+import DownloadDialogBox from "@/components/DownloadDialogBox";
+
+const TimelineNavbar = () => {
+  return (
+    <DownloadDialogBox className="md:flex items-center justify-between ms-24 px-8 w-[380px]   hidden">
+      <HomeIcon />
+      <SearchIcon />
+      <EventIcon />
+      <NotificationIcon />
+      <UserProfileIcon />
+    </DownloadDialogBox>
+  );
+};
+
+export default TimelineNavbar;
