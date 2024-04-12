4293938eba755292105257bb8ef807fca61fa5d9
Rayy-007 <minkhant88887@gmail.com>
2024-04-12 21:53:48 +0630

    feat: creat search screen

---
+++ apps/app/app/search/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchScreen from "./components/SearchScreen";
+
+export default function Home() {
+  return (
+    <div className="flex md:justify-center ">
+      <SearchScreen />
+    </div>
+  );
+}
