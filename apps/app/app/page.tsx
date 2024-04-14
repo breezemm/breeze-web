692e0305a36ed4927a8aa5d8343e187661f231bc
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-04-14 23:24:57 +0630

    refactor: remove dead code

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
 import FeedScreen from "@/app/components/FeedScreen";
 import React from "react";
-import {env} from "@/env";
 
 export default function Home() {
     return (
         <div className="flex md:justify-center ">
-            {env.NEXT_PUBLIC_BREEZE_API_URL}
             <FeedScreen/>
             <div>hey</div>
         </div>
